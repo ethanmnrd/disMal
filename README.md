c737301c2a3b4cfed9a7dcd7f01f253c4daa0054
Ethan Maynard <ethan.maynard@sjsu.edu>
2018-10-23 19:00:40 -0700

    initial commit; config.php and formatted README.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # disMal
-Web-based Antivirus application that allows a user to upload a file of any type and verify if the file contains malicious content.
+========
+
+- **disMal** is a Web-based Antivirus application that allows a user to upload a file of any type and verify if the file contains *malicious content*.
---
+++ resources/config.php
@@ -0,0 +1,42 @@
+<?php
+    $config = array(
+        "db" => array(
+            "db1" => array(
+                "dbname" => "disMaldB",
+                "username" => "user",
+                "password" => "pw",
+                "host" => "localhost"
+            ),
+            "db2" => array(
+                "dbname" => "database2",
+                "username" => "dbUser",
+                "password" => "pa$$",
+                "host" => "localhost"
+            )
+        ),
+        "urls" => array(
+            "baseUrl" => "http://www.disMal.net"
+        ),
+        "paths" => array(
+            "resources" => "../resources",
+            "images" => array(
+                "content" => $_SERVER["DOCUMENT_ROOT"] . "/images/content",
+                "layout" => $_SERVER["DOCUMENT_ROOT"] . "/images/layout"
+            )
+        )
+    );
+    
+    defined("LIBRARY_PATH")
+        or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/library'));
+         
+    defined("TEMPLATES_PATH")
+        or define("TEMPLATES_PATH", realpath(dirname(__FILE__) . '/templates'));
+     
+    /*
+        Error reporting.
+    */
+    ini_set("error_reporting", "true");
+    error_reporting(E_ALL|E_STRCT);
+     
+    ?>
+?>
