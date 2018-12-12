f21843724d562e9e8709da7655ccb8fd587f1a22
Ethan Maynard <ethan.maynard@sjsu.edu>
2018-12-12 14:13:01 -0800

    finished functionality

--- sig.php
+++ sig.php
@@ -1,33 +1,50 @@
 <?php
     require_once 'login.php';
     $conn = new mysqli($hn, $un, $pw, $db);
-    if ($conn->connect_error) die($conn->connect_error);
-    
-    $name = $sig = "";
+    if ($conn->connect_error) {
+        die($conn->connect_error);
+    }
+    mysqli_set_charset($conn, "utf8mb4");
+
+    $name = $sig = $fail = "";
 
     echo <<< _END
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
         <title>Malware Signature Upload</title>
-    </head>
 _END;
 
-    if (isset($_POST['logout']))
-    {
-        destroy_session_and_data();
-    } else {
-        session_start();
-    }
-    if (isset($_SESSION['username']))
-    {
+    session_start();
+    if (isset($_SESSION['username'])) {
+        if ($_SESSION['check'] != hash('ripemd128', $_SERVER['REMOTE_ADDR'] .$_SERVER['HTTP_USER_AGENT'])) {
+            different_user();
+        }
         echo <<< _END
+        <script>
+            function validate(form)
+            {
+                fail = validateName(form.name.value)
+
+                if (fail == "") return true
+                else { alert(fail); return false }
+            }
+
+            function validateName(field)
+            {
+                if (field == "") return "No malware name was entered.\n"
+                else if (/[^a-zA-Z0-9_-]/.test(field))
+                    return "Only A-Z, a-z, 0-9, - and _ allowed in malware names.\n"
+                return ""
+            }
+        </script>
+    </head>
     <body>
         <div class="center">
             <h1>Signature Upload</h1>
-            <form margin: 0 auto; action="sig.php" method="post" enctype='multipart/form-data'>
-                Name: <br><input type='text' name='name' size='25'><br><br>
-                Signature: <br><input type='text' name='sig' size='25'><br><br>
+            <form method="post" action="sig.php" onSubmit="return validate(this)" enctype='multipart/form-data'>
+                Malware Name: <br><input type='text' maxlength="20" name='name' value="$name" size='25'><br><br>    
+                Malware File: <br><br><input type='file' name='filename' size='20'>
                 <input type='submit' value='Upload'>
             </form>
             <form action="dismal.php" method="post" enctype='multipart/form-data'>
@@ -38,45 +55,68 @@ _END;
                 <button type="submit">Logout</button>
             </form>
 _END;
-
-        if (isset($_POST['name']) && isset($_POST['sig']))
-        {
+        /* Uploads and determines string signature (len 20 bytes)
+           Sanitizes malware name input
+           Sanitizes the signature prior to use */
+        if (isset($_POST['name']) && $_FILES) {
             $name = mysql_entities_fix_string($conn, $_POST['name']);
-            $sig = mysql_entities_fix_string($conn, $_POST['sig']);
 
-            $query = "INSERT INTO mal (name, sig) VALUES ('$name', '$sig');";
+            $fail .= validate_name($name);
 
-            $result = $conn->query($query);
+            if ($fail == "") {
+                $n = $_FILES['filename']['name'];
+                move_uploaded_file($_FILES['filename']['tmp_name'], 'uploads/'.$n) or die("<p><font color='red'><i>Must upload a file</i></font></p>");
 
-            if (!$result)
-            {
-                echo "<font color='red'>INSERT failed: $query<br>" . $conn->error . "</font><br><br></div></body></html>";
-            }
-            else
-            {
-                echo "<h4><font color='green'>Uploaded Malware signature successfully!</font></h4><br><br></div></body></html>";
+                $ha = fopen('uploads/'.$n, "rb") or die("<p><font color='red'><i>Could not open file</i></font></p>");
+                if (flock($ha, LOCK_EX)) {
+                    $data = unpack("H20sig", fread($ha, "20"));
+                    flock($ha, LOCK_UN);
+                }
+                fclose($ha);
+
+                unlink('uploads/'.$n) or die("<p><font color='red'><i>Could not delete file</i></font></p>");
+
+                $sig = mysql_entities_fix_string($conn, $data['sig']);
+
+                $query = "INSERT INTO mal (name, sig) VALUES ('$name', '$sig');";
+
+                $result = $conn->query($query);
+
+                if (!$result) {
+                    echo "<p><font color='red'><i>INSERT failed: $query<br>" . $conn->error . "</i></font></p><br><br>";
+                } else {
+                    echo "<p><font color='green'><i>Uploaded Malware signature $name : $sig successfully!</i></font></p><br><br>";
+                }
+            } else {
+                echo "<p><font color=red><i>$fail</i></font></p>";
             }
         }
     } else {
         echo <<< _END
+    </head>
     <body>
-        <form padding: 5; action="auth.php" method="post">
-            <input type="hidden" name="siglogin">
-            You must be <button type="submit">logged in</button> to view this page!
-        </form> 
-    </body>
-</html>
+        <div class=center>
+            <h1>
+                <form padding: 5; action="auth.php" method="post">
+                    <input type="hidden" name="siglogin">
+                    You must be <button type="submit">logged in</button> to view this page!
+                </form>
+            </h1>
 _END;
     }
 
+    echo "</div></body></html>";
+
     $conn->close();
 
-    function destroy_session_and_data()
+    function validate_name($field)
     {
-        session_start();
-        $_SESSION = array();
-        setcookie(session_name(), '', time() - 2592000, '/');
-        session_destroy();
+        if ($field == "") {
+            return "No malware name was entered.<br>";
+        } elseif (preg_match("/[^a-zA-Z0-9_-]/", $field)) {
+            return "Only A-Z, a-z, 0-9, - and _ allowed in malware names.<br>";
+        }
+        return "";
     }
 
     function mysql_entities_fix_string($conn, $string)
@@ -90,5 +130,15 @@ _END;
             $string = stripslashes($string);
         }
         return $conn->real_escape_string($string);
+    }
+
+    function different_user() {
+        $_SESSION = array();
+        setcookie(session_name(), '', time() - 2592000, '/');
+        session_destroy();
+        echo <<< _END
+        <div class='center'><form action="auth.php" method="post" enctype='multipart/form-data'>Error authenticating user: please <button type="submit">login</button> again!
+</form></div></body></html>
+_END;
     }
 ?>
