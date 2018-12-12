f41df76124b125e8a014bdc6c4bfb79f140d4014
Ethan Maynard <ethan.maynard@sjsu.edu>
2018-12-12 14:12:48 -0800

    finished functionality

--- dismal.php
+++ dismal.php
@@ -1,8 +1,13 @@
 <?php
     require_once 'login.php';
-    $connection = new mysqli($hn, $un, $pw, $db);
-    if ($connection->connect_error) die($connection->connect_error);
-    $username = $password = "";
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) {
+        die($conn->connect_error);
+    }
+    mysqli_set_charset($conn, "utf8mb4");
+
+    $username = $password = $sig = "";
+    
     echo <<<_END
 <html>
     <head>
@@ -14,19 +19,20 @@
             <h1>disMal Malware Check</h1>
 
             <form margin: 0 auto; action="dismal.php" method="post" enctype='multipart/form-data'>
-                Select File <input type='file' name='filename' size='10'>
-                            <input type='submit' value='Upload'>
+                Questionable File:<br><br><input type='file' name='filename' size='20'>
+                <input type='submit' value='Upload'>
             </form>
 _END;
     
-    if (isset($_POST['logout']))
-    {
+    if (isset($_POST['logout'])) {
         destroy_session_and_data();
     } else {
         session_start();
     }
-    if (isset($_SESSION['username']))
-    {
+    if (isset($_SESSION['username'])) {
+        if ($_SESSION['check'] != hash('ripemd128', $_SERVER['REMOTE_ADDR'] .$_SERVER['HTTP_USER_AGENT'])) {
+            different_user();
+        }
         $username = $_SESSION['username'];
         $password = $_SESSION['password'];
 
@@ -38,29 +44,57 @@ _END;
                 <input type="hidden" name='logout' value='yes'>
                 <button type="submit">Logout</button>
             </form>
-        </div>
-    </body>
-</html> 
 _END;
     } else {
         echo <<< _END
             <form action="auth.php" method="post" enctype='multipart/form-data'>
                 <button type="submit">Staff Login</button>
             </form>
-        </div>
-    </body>
-</html>
 _END;
     }
 
-    if ($_FILES)
-    {
-        $name = "";
-        $name = $_FILES['filename']['name'] or die ("Must upload a file.");
+    if ($_FILES) {
+        $n = $_FILES['filename']['name'];
+        move_uploaded_file($_FILES['filename']['tmp_name'], 'checks/'.$n) or die("<p><font color='red'><i>Must upload a file</i></font></p>");
         
+        $ha = fopen('checks/'.$n, "rb") or die("<p><font color='red'><i>Could not open file</i></font></p>");
+        if (flock($ha, LOCK_EX)) {
+            $data = unpack("H20sig", fread($ha, "20"));
+            flock($ha, LOCK_UN);
+        }
+        fclose($ha);
+
+        unlink('checks/'.$n) or die("<p><font color='red'><i>Could not delete file</i></font></p>");
+
+        $sig = mysql_entities_fix_string($conn, $data['sig']);
+
+        $query = "SELECT * FROM mal WHERE sig='$sig';";
+
+        $result = $conn->query($query);
+
+        if (!$result) {
+            echo "<p><font color='red'><i>SELECT failed: $query<br>" . $conn->error . "</i></font></p><br><br>";
+        } else {
+            $rows = $result->num_rows;
+
+            if ($rows == 0) {
+                echo "<p><font color='green'><i>File searched successfully! Your file is clean!</i></font></p><br><br>";
+            } else {
+                echo "<p><font color='red'><i>Your file is infected by one of the following:<br>";
+                for ($i = 0; $i < $rows; ++$i) {
+                    $result->data_seek($i);
+                    $row = $result->fetch_array(MYSQLI_NUM);
+                    echo $row[0] . "<br>";
+                }
+                echo "</i></font></p>";
+            }
+        }
+        $result->close();
     }
 
-    $connection->close();
+    echo "</div></body></html>";
+
+    $conn->close();
 
     function destroy_session_and_data()
     {
@@ -81,5 +115,15 @@ _END;
             $string = stripslashes($string);
         }
         return $conn->real_escape_string($string);
+    }
+
+    function different_user() {
+        $_SESSION = array();
+        setcookie(session_name(), '', time() - 2592000, '/');
+        session_destroy();
+        echo <<< _END
+        <form action="auth.php" method="post" enctype='multipart/form-data'>Error authenticating user: please <button type="submit">login</button> again!
+</form>
+_END;
     }
 ?>
