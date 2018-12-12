aefc2372382dec37346cac3402bad3ddd7f58077
Ethan Maynard <ethan.maynard@sjsu.edu>
2018-12-12 14:12:37 -0800

    secured session authentication

--- auth.php
+++ auth.php
@@ -1,7 +1,9 @@
 <?php // auth.php
     require_once 'login.php';
-    $connection = new mysqli($hn, $un, $pw, $db);
-    if ($connection->connect_error) die($connection->connect_error);
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if ($conn->connect_error) {
+        die($conn->connect_error);
+    }
     
     echo <<< _END
 <html>
@@ -13,62 +15,67 @@
     <div class="center">
 _END;
 
-    if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW']))
-    {
-        $un_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_USER']);
-        $pw_temp = mysql_entities_fix_string($connection, $_SERVER['PHP_AUTH_PW']);
+    if (isset($_SERVER['PHP_AUTH_USER']) && isset($_SERVER['PHP_AUTH_PW'])) {
+        $un_temp = mysql_entities_fix_string($conn, $_SERVER['PHP_AUTH_USER']);
+        $pw_temp = mysql_entities_fix_string($conn, $_SERVER['PHP_AUTH_PW']);
         
         $query = "SELECT * FROM users WHERE username='$un_temp'";
-        $result = $connection->query($query);
+        $result = $conn->query($query);
         
-        if (!$result) die($connection->error);
-        elseif ($result->num_rows)
-        {
+        if (!$result) {
+            die($conn->error);
+        } elseif ($result->num_rows) {
             $row = $result->fetch_array(MYSQLI_NUM);
             $result->close();
-            $salt1 = "qm&h*"; $salt2 = "pg!@";
+            $salt1 = "qm&h*";
+            $salt2 = "pg!@";
             $token = hash('ripemd128', "$salt1$pw_temp$salt2");
             
-            if ($token == $row[2])
-            {
+            if ($token == $row[2]) {
+                ini_set('session.gc_maxlifetime', 60 * 60 * 24);
                 session_start();
+                if (!isset($_SESSION['initiated']))
+                {
+                    session_regenerate_id();
+                    $_SESSION['initiated'] = 1;
+                }
                 $_SESSION['username'] = $un_temp;
                 $_SESSION['password'] = $pw_temp;
+                $_SESSION['check'] = hash('ripemd128', $_SERVER['REMOTE_ADDR'] .$_SERVER['HTTP_USER_AGENT']);
                 
-                if(isset($_POST['siglogin']))
-                {
+                if (isset($_POST['siglogin'])) {
                     echo "<form action='sig.php' method='post'>";
-                }
-                else
-                {
+                } else {
                     echo "<form action='dismal.php' method='post'>";
                 }
-                echo "You are now logged in as '$row[1]'<br><br>
-                        <button type='submit'>Click here to continue</button> 
+                echo "<h3>You are now logged in as '$row[1]'<br><br>
+                        <button type='submit'>Click here to continue</button></h3>
                     </form>";
+            } else {
+                die("Invalid username/password combination");
             }
-            else die("Invalid username/password combination");
+        } else {
+            die("Invalid username/password combination");
         }
-        else die("Invalid username/password combination");
-    }
-    else
-    { // if ($_SERVER['PHP_AUTH_USER’])  and  ($_SERVER['PHP_AUTH_PW’]) are not set
+    } else {
         header('WWW-Authenticate: Basic realm="Restricted Section"');
         header('HTTP/1.0 401 Unauthorized');
-        die ("Please enter your username and password");
+        die("Please enter your username and password");
     }
-    $connection->close();
+    $conn->close();
     
     echo "</div></body></html>";
 
-    function mysql_entities_fix_string($connection, $string)
+    function mysql_entities_fix_string($conn, $string)
     {
-        return htmlentities(mysql_fix_string($connection, $string));
+        return htmlentities(mysql_fix_string($conn, $string));
     }
     
-    function mysql_fix_string($connection, $string)
+    function mysql_fix_string($conn, $string)
     {
-        if (get_magic_quotes_gpc()) $string = stripslashes($string);
-            return $connection->real_escape_string($string);
+        if (get_magic_quotes_gpc()) {
+            $string = stripslashes($string);
+        }
+        return $conn->real_escape_string($string);
     }
-?> 
+?>
